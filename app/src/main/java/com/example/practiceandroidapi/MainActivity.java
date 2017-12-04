fe02d0a71b6a6259d36e29dc399e006045827426
tomotaka.kato <tomotaka_kato@CCT1411-002.local>
2017-12-04 09:45:32 +0900

    Timer画面へのページ遷移実装

--- app/src/main/java/com/example/practiceandroidapi/MainActivity.java
+++ app/src/main/java/com/example/practiceandroidapi/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.practiceandroidapi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Go to Timer page.
+     * Called when the user taps the Go To Timer Button.
+     */
+    public void goToTimer(View view) {
+        Intent intent = new Intent(this, TimerActivity.class);
+        startActivity(intent);
+    }
+
 }
